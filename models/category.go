0e16f9b7eba0c73ef3bb8e2492dd3085c41900d9
Jeremia Axel <jeremiaaxelb@gmail.com>
2023-12-15 12:05:23 +0700

    fix(category): add optional is_expense

--- models/category.go
+++ models/category.go
@@ -8,7 +8,7 @@ type Category struct {
 	Priority  int       `json:"priority"`
 	Budget    float64   `json:"budget"`
 	NoteId    uuid.UUID `json:"note_id"`
-	IsExpense bool      `json:"is_expense"`
+	IsExpense bool      `json:"is_expense" gorm:"default:true; not null"`
 
 	Note Note `gorm:"foreignKey:NoteId"`
 }
